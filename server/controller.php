3cf20aaf7459dbce6f694f503c1476126e44f3f2
Onillon Adrien <onillon4@mmi-dev.unilim.fr>
2025-04-04 18:41:07 +0200

    back iteration 2 n°6

--- server/controller.php
+++ server/controller.php
@@ -1,23 +1,6 @@
 <?php
 
-/** ARCHITECTURE PHP SERVEUR  : Rôle du fichier controller.php
- * 
- *  Dans ce fichier, on va définir les fonctions de contrôle qui vont traiter les requêtes HTTP.
- *  Les requêtes HTTP sont interprétées selon la valeur du paramètre 'todo' de la requête (voir script.php)
- *  Pour chaque valeur différente, on déclarera une fonction de contrôle différente.
- * 
- *  Les fonctions de contrôle vont éventuellement lire les paramètres additionnels de la requête, 
- *  les vérifier, puis appeler les fonctions du modèle (model.php) pour effectuer les opérations
- *  nécessaires sur la base de données.
- *  
- *  Si la fonction échoue à traiter la requête, elle retourne false (mauvais paramètres, erreur de connexion à la BDD, etc.)
- *  Sinon elle retourne le résultat de l'opération (des données ou un message) à includre dans la réponse HTTP.
- */
 
-/** Inclusion du fichier model.php
- *  Pour pouvoir utiliser les fonctions qui y sont déclarées et qui permettent
- *  de faire des opérations sur les données stockées en base de données.
- */
 require("model.php");
 
 function readMoviesController(){
@@ -27,10 +10,7 @@ function readMoviesController(){
 
 
 function addController(){
-    /* Lecture des données de formulaire
-      On ne vérifie pas si les données sont valides, on suppose (faudra pas toujours...) que le client les a déjà
-      vérifiées avant de les envoyer 
-    */
+
 
     // header('Content-Type: application/json');
     // echo json_encode($_REQUEST);
@@ -47,13 +27,13 @@ function addController(){
     $min_age = $_REQUEST['min_age'];
 
 
-    // Vérification des données
+  
     if (empty($name) || empty($year) || empty($length) || empty($description) || empty($director) || empty($id_category) || empty($image) || empty($trailer) || empty($min_age)) {
         return "Erreur : tous les champs sont obligatoires !";
     }
-    // Mise à jour du menu à l'aide de la fonction updateMenu décrite dans model.php
+ 
     $ok = addMovie($name, $year, $length, $description, $director, $id_category, $image, $trailer, $min_age); 
-    // $ok est le nombre de ligne affecté par l'opération de mise à jour dans la BDD (voir model.php)
+
     if ($ok!=0){
       return "Le film $name est à été ajouté avec succès";
     }
--- server/model.php
+++ server/model.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * Ce fichier contient toutes les fonctions qui réalisent des opérations
- * sur la base de données, telles que les requêtes SQL pour insérer, 
- * mettre à jour, supprimer ou récupérer des données.
- */
 
-/**
- * Définition des constantes de connexion à la base de données.
- *
- * HOST : Nom d'hôte du serveur de base de données, ici "localhost".
- * DBNAME : Nom de la base de données
- * DBLOGIN : Nom d'utilisateur pour se connecter à la base de données.
- * DBPWD : Mot de passe pour se connecter à la base de données.
- */
 define("HOST", "localhost");
 define("DBNAME", "onillon4");
 define("DBLOGIN", "onillon4");
@@ -20,29 +7,29 @@ define("DBPWD", "onillon4");
 
 
 function getAllMovies(){
-    // Connexion à la base de données
+  
     $cnx = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DBLOGIN, DBPWD);
-    // Requête SQL pour récupérer le menu avec des paramètres
+    
     $sql = "select id, name, image from Movie";
-    // Prépare la requête SQL
+    
     $stmt = $cnx->prepare($sql);;
-    // Exécute la requête SQL
+  
     $stmt->execute();
-    // Récupère les résultats de la requête sous forme d'objets
+
     $res = $stmt->fetchAll(PDO::FETCH_OBJ);
-    return $res; // Retourne les résultats
+    return $res;
 }
 
 
 function addMovie($n, $y, $l, $de, $di, $id, $img, $t, $m){
-    // Connexion à la base de données
+   
     $cnx = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DBLOGIN, DBPWD); 
-    // Requête SQL de mise à jour du menu avec des paramètres
+   
     $sql = "INSERT INTO Movie (name, year, length, description, director, id_category, image, trailer, min_age) 
             VALUES (:name, :year, :length, :description, :director, :id_category, :image, :trailer, :min_age)";
-    // Prépare la requête SQL
+  
     $stmt = $cnx->prepare($sql);
-    // Lie les paramètres aux valeurs
+  
     $stmt->bindParam(':name', $n);
     $stmt->bindParam(':year', $y);
     $stmt->bindParam(':length', $l);
@@ -52,10 +39,10 @@ function addMovie($n, $y, $l, $de, $di, $id, $img, $t, $m){
     $stmt->bindParam(':image', $img);
     $stmt->bindParam(':trailer', $t);
     $stmt->bindParam(':min_age', $m);
-    // Exécute la requête SQL
+
     $stmt->execute();
-    // Récupère le nombre de lignes affectées par la requête
+
     $res = $stmt->rowCount(); 
-    return $res; // Retourne le nombre de lignes affectées
+    return $res; 
 }
 
