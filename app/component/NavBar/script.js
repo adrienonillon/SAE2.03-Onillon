82cb09ae54142c4f4e6fa2a6f93b79783265b629
Onillon Adrien <onillon4@mmi-dev.unilim.fr>
2025-04-09 16:05:34 +0200

    iteration 6 fini probleme css résolu

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -1,51 +1,24 @@
-// let templateFile = await fetch("./component/NavBar/template.html");
-// let template = await templateFile.text();
-
-// let NavBar = {};
-
-// NavBar.format = function (hHome, hProfile) {
-//   let html = template;
-//   html = html.replace("{{hHome}}", hHome);
-//   html = html.replace("{{hProfile}}", hProfile);
-//   return html;
-// };
-
-// export { NavBar };
-
 let templateFile = await fetch("./component/NavBar/template.html");
 let template = await templateFile.text();
 
-const templateFile2 = await fetch("./component/NavBar/template2.html");
-const template2 = await templateFile2.text();
-
-
 let NavBar = {};
 
-NavBar.format = function ( hHome, hProfile, activeProfileId ) {
+NavBar.format = function (hHome, hProfile, profiles) {
   let html = template;
+  html = html.replace("{{hHome}}", hHome);
+  html = html.replace("{{handler}}", hProfile);
 
-
-  let optionsHTML = "";
-  
-  for (let profile of hProfile) {
-    let option = template2;
-    option = option.replace("{{name}}", profile.name);
-    option = option.replace("{{id}}", profile.id);
-
-    if (profile.id == activeProfileId) {
-      option = option.replace(">", " selected>");
-    }
-
-    optionsHTML += option;
+  let profile = "";
+  for (let i = 0; i < profiles.length; i++) {
+    let p = profiles[i];
+    profile += `<option value="${p.name}" data-img="${p.image}" data-dob="${p.age}">${p.name}</option>`;
   }
 
-  html = html.replace("{{hHome}}", hHome);
-  html = html.replace("{{hProfile}}", optionsHTML);
-
+  let image = profiles[0]?.image || "";
+  html = html.replace("{{profile}}", profile);
+  html = html.replace("{{image}}", image);
 
   return html;
 };
-
-
 
 export { NavBar };
